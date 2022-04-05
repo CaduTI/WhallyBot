d3cb9facd40cf9592e415e25868d8ca7a5115101
CaduTI <caduti0201@gmail.com>
2022-04-05 18:51:06 -0300

    Version 0.2
    Atualizamos os comentários em relação a função do XML e ajeitei o direcionamento da função que pega o nome da moeda para a função certa.

--- BotProjeto/main.py
+++ BotProjeto/main.py
@@ -2,45 +2,57 @@ import json
 import requests
 import telebot
 
+
 class Moeda:
-    def __init__(self,nome):
+    def __init__(self, nome):
         self.nome = nome
-        self.valor= None
+        self.valor = None
 
 
 moeda_dict = {}
 user = {}
 api_key = "5191904654:AAFrD8ybCk8N06Xx4yVbSH2r-hsxQ6k7jxE"
 bot = telebot.TeleBot(api_key)
+
+
+
 @bot.message_handler(commands=["listarmoedas"])
 def pegar_moeda(message):#pego a moeda que o usuário deseja
-    #try:
-        msg= message
+    try:
+        msg = message
         bot.send_message(msg.chat.id,"digite a moeda deseja")
-        bot.register_next_step_handler(msg,consultar_cotacao)
-    #except Exception as e:
-     #   print(e)
+        bot.register_next_step_handler(msg,procura_tagmoeda_arquivo)
+    except Exception :
+       print(Exception)
 
 def procura_tagmoeda_arquivo(message):#procuro dentro do XML a tag da moeda enviada pelo usuário
-    pass
+    #pass
+    try:
+        pass
+    #vai fazer a BUSCA DENTRO DO XML com o nome da moeds ue o cliente digitou, caso ele n ache vai pro except
+    except:
+        pass
+        #RaiseException("Deu ruim patrão, digita certo ae")
 def consultar_cotacao(message):
-    # try:
-    msg= message
-    moedanome= message.text
-    moeda = Moeda(moedanome)
-    print(moeda)
-    request = requests.get('https://economia.awesomeapi.com.br/last/USD-{}'.format(moeda))
-    moeda_info = request.json()
-    bot.reply_to(msg, "Segue a cotação da moeda:")
-    if 'erro' not in moeda_info:
-        bot.send_message(format(moeda_info.bid))
+     try:
+         msg = message
+         moedanome = msg.text
+         print(moedanome)
+         if True:
+            request = requests.get('https://economia.awesomeapi.com.br/last/USD-{}'.format(moedanome))
+            moeda_info = request.json()
+            print(moeda_info)
+            print(request)
+            
+            bot.reply_to(msg, f"Segue a cotação da moeda:{moeda_info}")
+     except:
+         print(Exception)
 
-    """except Exception as e:
-        print(e)"""
+
 
 @bot.message_handler(commands=['test'])
 def teste(message):
-    resposta_user= message.text
+    resposta_user = message.text
     print("texto da mensagem:", resposta_user)
     print("id do usuário", message.chat.id)
 
