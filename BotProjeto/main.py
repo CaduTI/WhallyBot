2bda9d0055df775c3675ce9634aa1e92d2014eeb
CaduTI <caduti0201@gmail.com>
2022-04-09 18:56:09 -0300

    Version 0.4
    Dei início  a função de procura no xml qual a tag da moeda pesquisada(inacabado), o arquivo está sendo aberto(a corrigir).

--- BotProjeto/main.py
+++ BotProjeto/main.py
@@ -1,6 +1,6 @@
-import json
 import requests
 import telebot
+from xml.dom import minidom
 
 
 class Moeda:
@@ -15,39 +15,44 @@ api_key = "5191904654:AAFrD8ybCk8N06Xx4yVbSH2r-hsxQ6k7jxE"
 bot = telebot.TeleBot(api_key)
 
 
-
 @bot.message_handler(commands=["listarmoedas"])
-def pegar_moeda(message):#pego a moeda que o usuário deseja
+def pegar_moeda(message):
+    """pego a moeda que o usuário deseja"""
     try:
-        msg = message
-        bot.send_message(msg.chat.id,"digite a moeda deseja")
-        bot.register_next_step_handler(msg,procura_tagmoeda_arquivo)
-    except Exception :
-       print(Exception)
+        bot.send_message(message.chat.id, "digite a moeda deseja")
+        bot.register_next_step_handler(message, procura_tagmoeda_arquivo)
+    except Exception as e:
+       print(e)
+
 
 def procura_tagmoeda_arquivo(message):#procuro dentro do XML a tag da moeda enviada pelo usuário
-    #pass
     try:
-        pass
+        moedanome = message.text
+        with open('tagmoedas.xml','r', enconding='utf-8') as f:
+            xml = minidom.parse(f)
+            #moeda_nome = xml.getElementstree(moedanome)
+
+            for tag in xml:
+                if f [''] == moedanome:
+                    print(f['0'])
     #vai fazer a BUSCA DENTRO DO XML com o nome da moeds ue o cliente digitou, caso ele n ache vai pro except
-    except:
-        pass
+    except Exception as e:
+        print(e)
         #RaiseException("Deu ruim patrão, digita certo ae")
 def consultar_cotacao(message):
      try:
-         msg = message
-         moedanome = msg.text
-         print(moedanome)
-         if True:
-            request = requests.get('https://economia.awesomeapi.com.br/last/USD-{}'.format(moedanome))
+
+        tagmoeda = message.text
+        print(tagmoeda)
+        if True:
+            request = requests.get('https://economia.awesomeapi.com.br/last/USD-{}'.format(tagmoeda))
             moeda_info = request.json()
             print(moeda_info)
             print(request)
             
-            bot.reply_to(msg, f"Segue a cotação da moeda:{moeda_info}")
-     except:
-         print(Exception)
-
+            bot.reply_to(message, "Segue a cotação da moeda:{}".format(moeda_info['USD']['bid']))
+     except Exception as e:
+         print(e)
 
 
 @bot.message_handler(commands=['test'])
